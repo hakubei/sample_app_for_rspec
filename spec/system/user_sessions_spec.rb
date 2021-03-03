46eb75e090ced8670e1376f6468093956e3ea3a4
hakubei <sho1018ta@icloud.com>
2021-03-04 04:50:59 +0900

    user_sessionsのsystem specの作成

--- spec/system/user_sessions_spec.rb
+++ spec/system/user_sessions_spec.rb
@@ -1,18 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe 'UserSessions', type: :system do
-  # describe 'ログイン前' do
-  #   context 'フォームの入力値が正常' do
-  #     it 'ログイン処理が成功する'
-  #   end
-  #   context 'フォームが未入力' do
-  #     it 'ログイン処理が失敗する'
-  #   end
-  # end
+  let(:user) { create(:user) }
 
-  # describe 'ログイン後' do
-  #   context 'ログアウトボタンをクリック' do
-  #     it 'ログアウト処理が成功する'
-  #   end
-  # end
+  describe 'ログイン前' do
+    context 'フォームの入力値が正常' do
+      it 'ログイン処理が成功する' do
+        visit login_path
+        fill_in 'Email', with: user.email
+        fill_in 'Password', with: 'password'
+        click_button 'Login'
+        expect(page).to have_content 'Login successful'
+        expect(current_path).to eq root_path
+      end
+    end
+    context 'フォームが未入力' do
+      it 'ログイン処理が失敗する' do
+        visit login_path
+        fill_in 'Email', with: ''
+        fill_in 'Password', with: 'password'
+        click_button 'Login'
+        expect(page).to have_content 'Login failed'
+        expect(current_path).to eq login_path
+      end
+    end
+  end
+
+  describe 'ログイン後' do
+    context 'ログアウトボタンをクリック' do
+      it 'ログアウト処理が成功する' do
+        visit login_path
+        fill_in 'Email', with: user.email
+        fill_in 'Password', with: 'password'
+        click_button 'Login'
+        click_link 'Logout'
+        expect(page).to have_content 'Logged out'
+        expect(current_path).to eq root_path
+      end
+    end
+  end
 end
